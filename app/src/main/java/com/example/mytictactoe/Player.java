ba4cf9fe3eb829b2b1271ed4debd29a935377668
JensLantz <44167121+JensLantz@users.noreply.github.com>
2020-03-10 16:07:24 +0100

    Added datamodels

---
+++ app/src/main/java/com/example/mytictactoe/Player.java
@@ -0,0 +1,13 @@
+package com.example.mytictactoe;
+
+public class Player {
+
+    public String name;
+    public String value;
+
+    public Player(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+}
